b8ec09f61b7865d27ebbcd6c08d09728c36fe489
Hristina Hristova <hr.hristovaa@gmail.com>
2024-11-22 18:56:47 +0200

    WIP dashboard cards

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,7 +13,7 @@ export default function AdminLayout({
             <NavLink href="/admin/users">Customers</NavLink>
             <NavLink href="/admin/orders">Sales</NavLink>
         </Nav>
-        <div className="container my-6">
+        <div className="container m-6">
             {children}
         </div>
     </>
